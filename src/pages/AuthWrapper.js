a3e8b8e2d803a4e27a658093a50eccb3447124c2
Razieh Abedi <raziyeabedi@gmail.com>
2022-12-05 14:04:27 +0800

    did the inistial app setup

---
+++ src/pages/AuthWrapper.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function AuthWrapper() {
+  return <div>AuthWrapper</div>;
+}
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+`;
+
+export default AuthWrapper;
---
+++ src/pages/CartPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+function CartPage() {
+  return <div>CartPage</div>;
+}
+const Wrapper = styled.main`
+  .empty {
+    text-align: center;
+    h2 {
+      margin-bottom: 1rem;
+      text-transform: none;
+    }
+  }
+`;
+export default CartPage;
