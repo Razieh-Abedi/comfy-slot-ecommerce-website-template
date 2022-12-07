80dbed3117b1270f527f43160f92dd8468f47874
Razieh Abedi <raziyeabedi@gmail.com>
2022-12-07 12:37:08 +0800

    set up navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 import styled from "styled-components";
 import { FaBars } from "react-icons/fa";
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { links } from "../utils/constants";
+import CartButtons from "./CartButtons";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <NavContainer>
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="Comfy Sloth Logo" />
+          </Link>
+          <button type="button" className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {links.map((link) => {
+            const { id, text, url } = link;
+            return <li key={id}>
+              <Link to={url}>
+                {text}
+              </Link>
+            </li>;
+          })}
+        </ul>
+        <CartButtons/>
+      </div>
+    </NavContainer>
+  );
 }
 
 const NavContainer = styled.nav`
