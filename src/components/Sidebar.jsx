d8c35a5e09bcf89478bf34f381e7fba7fc1c6a5b
Razieh Abedi <raziyeabedi@gmail.com>
2022-12-07 15:43:06 +0800

    setup context for sidebar state

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,8 +5,11 @@ import logo from "../assets/logo.svg";
 import { Link } from "react-router-dom";
 import { links } from "../utils/constants";
 import CartButtons from "./CartButtons";
+import { useProductsContext } from "../context/products_context";
 
 function Sidebar() {
+  const data = useProductsContext();
+  console.log(data);
   const isOpen = false;
   return (
     <SidebarContainer>
@@ -30,7 +33,7 @@ function Sidebar() {
             <Link to="/checkout">Checkout</Link>
           </li>
         </ul>
-        <CartButtons/>
+        <CartButtons />
       </aside>
     </SidebarContainer>
   );
