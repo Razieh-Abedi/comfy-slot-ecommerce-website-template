d127c4ea1eeea89d72968a01a5bcbb0f7e493601
Razieh Abedi <raziyeabedi@gmail.com>
2022-12-08 11:45:41 +0800

    setup services section in homepage

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import styled from "styled-components";
+import { services } from "../utils/constants";
 
 function Services() {
-  return <div>Services</div>;
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <article className="header">
+          <h3>
+            custom furniture <br /> built only for you
+          </h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            veniam ducimus dignissimos atque magnam? Laudantium amet eligendi
+            magni dignissimos ullam!
+          </p>
+        </article>
+        <div className="services-center">
+          {services.map((service) => {
+            const { id, icon, title, text } = service;
+            return (
+              <article key={id} className="service">
+                <span className="icon">{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.section`
