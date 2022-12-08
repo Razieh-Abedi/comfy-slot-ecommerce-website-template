68e2124e6e4fb55fede0856e804230ce7a742502
Razieh Abedi <raziyeabedi@gmail.com>
2022-12-08 16:28:50 +0800

    fetched single product page url

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { useParams, useNavigate } from "react-router-dom";
+import { single_product_url as url } from "../utils/constants";
+import { useProductsContext } from "../context/products_context";
+import { formatPrice } from "../utils/helpers";
+import { Link } from "react-router-dom";
+import {
+  Loading,
+  Error,
+  ProductImages,
+  AddToCart,
+  Stars,
+  PageHero,
+} from "../components";
 
 function SingleProductPage() {
   const { id } = useParams();
+  const {
+    fetchSingleProduct,
+    single_product_loading: loading,
+    single_product_error: error,
+    single_product: product,
+  } = useProductsContext();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`);
+  }, [id]);
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    }
+  }, [error]);
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <Error />;
+  }
+
   return <div>SingleProductPage</div>;
 }
 
