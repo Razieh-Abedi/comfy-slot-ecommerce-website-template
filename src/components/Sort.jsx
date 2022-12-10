95f8e7f51979ff6914dd87c578d3671160015173
Razieh Abedi <raziyeabedi@gmail.com>
2022-12-10 15:09:43 +0800

    did basic setup for sort component in products page

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 import { BsFillGridFill, BsList } from "react-icons/bs";
+import { useFilterContext } from "../context/filter_context";
+import products_reducer from "../reducers/products_reducer";
 
 function Sort() {
-  return <div>Sort</div>;
+  const { filtered_products: products, grid_view } = useFilterContext();
+  return (
+    <Wrapper>
+      <div className="btn-container">
+        <button type="button" className={`${grid_view ? "active" : null}`}>
+          <BsFillGridFill />
+        </button>
+        <button type="button" className={`${!grid_view ? "active" : null}`}>
+          <BsList />
+        </button>
+      </div>
+      <p>{products.length} products found</p>
+      <hr />
+      <form>
+        <label htmlFor="sort">sort by</label>
+        <select name="sort" id="sort" className="sort-input">
+          <option value="price-lowest">price (lowest)</option>
+          <option value="price-highest">price (highest)</option>
+          <option value="name-a">name (a-z)</option>
+          <option value="name-z">name (z-a)</option>
+        </select>
+      </form>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.section`
