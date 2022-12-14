182989c386a63123b1d8429344e168b31896f79e
Razieh Abedi <raziyeabedi@gmail.com>
2022-12-14 17:26:49 +0800

    set functionality for calcluate total of cart page

--- src/components/CartButtons.jsx
+++ src/components/CartButtons.jsx
@@ -3,16 +3,18 @@ import styled from "styled-components";
 import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useProductsContext } from "../context/products_context";
+import { useCartContext } from "../context/cart_context";
 
 function CartButtons() {
   const { closeSidebar } = useProductsContext();
+  const { total_items } = useCartContext();
   return (
     <Wrapper className="cart-btn-wrapper">
       <Link to="/cart" className="cart-btn" onClick={closeSidebar}>
         Cart
         <span className="cart-container">
           <FaShoppingCart />
-          <span className="cart-value">12</span>
+          <span className="cart-value">{total_items}</span>
         </span>
       </Link>
       <button type="button" className="auth-btn">
